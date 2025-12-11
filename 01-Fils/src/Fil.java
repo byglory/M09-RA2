89ff52dfda9e935b6f99fac04b998891d87d0c41
itzan jimenez <holaitzan@gmail.com>
2025-12-11 19:54:58 +0100

    fi de clase

--- 01-Fils/src/Fil.java
+++ 01-Fils/src/Fil.java
@@ -9,7 +9,7 @@ public class Fil extends Thread{
             
         for (int i = 0; i <= 9; i++) {
             System.out.println(this.nom + " " + i);
-            sleep(1000);
+            sleep(1);
         }
         System.out.println("Acaba el fil " + this.nom);
         } catch (Exception e) {
